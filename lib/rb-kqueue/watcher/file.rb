8f740785cc3707bd3fae0761168e98347a893ccd
Pavel Argentov <argentoff@gmail.com>
2012-10-17 17:36:35 +0400

    Fixed the wrong name again

--- lib/rb-kqueue/watcher/file.rb
+++ lib/rb-kqueue/watcher/file.rb
@@ -49,7 +49,7 @@ module KQueue
               end,
               FFI.errno)
           end)
-        super(queue, @path.fileno, :vnode, flags, nil, callback)
+        super(queue, @fd.fileno, :vnode, flags, nil, callback)
       end
     end
   end
