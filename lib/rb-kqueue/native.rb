464d95fc5a3420a6872293f04c6192f7355552b5
r-stu31 <r.stu3.1@googlemail.com>
2014-06-30 08:23:04 +0200

    Use types in agreement with the event.h of native operating systems.

--- lib/rb-kqueue/native.rb
+++ lib/rb-kqueue/native.rb
@@ -13,13 +13,39 @@ module KQueue
     #
     # @private
     class KEvent < FFI::Struct
-      layout(
-        :ident,  :uintptr_t,
-        :filter, :int16,
-        :flags,  :uint16,
-        :fflags, :uint32,
-        :data,   :intptr_t,
-        :udata,  :pointer)
+      if FFI::Platform::IS_FREEBSD
+        layout(
+          :ident,  :uintptr_t,
+          :filter, :short,
+          :flags,  :u_short,
+          :fflags, :u_int,
+          :data,   :intptr_t,
+          :udata,  :pointer)
+      elsif FFI::Platform::IS_NETBSD
+        layout(
+          :ident,  :uintptr_t,
+          :filter, :uint32_t,
+          :flags,  :uint32_t,
+          :fflags, :uint32_t,
+          :data,   :int64_t,
+          :udata,  :pointer)
+      elsif FFI::Platform::IS_OPENBSD
+        layout(
+          :ident,  :__uintptr_t,
+          :filter, :short,
+          :flags,  :u_short,
+          :fflags, :u_int,
+          :data,   :quad_t,
+          :udata,  :pointer)
+      else
+        layout(
+          :ident,  :uintptr_t,
+          :filter, :int16,
+          :flags,  :uint16,
+          :fflags, :uint32,
+          :data,   :intptr_t,
+          :udata,  :pointer)
+      end
     end
 
     # The C struct describing a timeout.
